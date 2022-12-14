6b3f5c20c6de30d15df4f162c8693e206b4b7962
Matheus Cogo <matheuscarvalhocogo@gmail.com>
2022-12-14 10:31:43 -0300

    ajustes

--- src/pages/CadastrarPlano/components/FormPlano.js
+++ src/pages/CadastrarPlano/components/FormPlano.js
@@ -3,7 +3,7 @@ export default function FormPlano() {
     <div>
       <iframe
         title="myFrame"
-        src="http://localhost:5000/cadastrarPlano.html"
+        src="http://192.168.214.131:5000/cadastrarPlano.html"
         width={1000}
         height={1000}
         frameBorder="0"
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api/v1/',
+  baseURL: 'http://192.168.214.131:5000/api/v1/',
   // baseURL: 'https://matheuscogo.pythonanywhere.com/api/v1/',
 })
 
